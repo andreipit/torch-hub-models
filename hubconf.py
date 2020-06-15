10cc3acd21538a27259c98746d936bddff6cbdd2
andreipit <andrei.pit@icloud.com>
2020-06-15 16:20:37 +0300

    init

---
+++ hubconf.py
@@ -0,0 +1,52 @@
+dependencies = ['torch']
+from torchvision.models.resnet import resnet18 as _resnet18
+
+# resnet18 is the name of entrypoint
+def resnet18(pretrained=False, **kwargs):
+    """ # This docstring shows up in hub.help()
+    Resnet18 model
+    pretrained (bool): kwargs, load pretrained weights into the model
+    """
+    # Call the model, load pretrained weights
+
+    class Net(nn.Module):
+        def __init__(self, _input_size=(3,28,28)):
+            super(Net, self).__init__()
+            #torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
+            _filter_size = 5
+            _padding = (int)((_filter_size - 1) / 2) # add zeros at borders -> to keep resolution
+            _stride = 1 # do nothing (gap between filter applying)
+            _in = 3
+            _out = 6
+            self.conv1 = nn.Conv2d(in_channels=_in, out_channels=_out, kernel_size=(_filter_size,_filter_size),padding=(_padding,_padding), stride=(_stride,_stride)) 
+            _pool_size=2
+            self.pool = nn.MaxPool2d(_pool_size, _pool_size)
+    #         self.conv2 = nn.Conv2d(6, 16, 5)
+            #self.fc1 = nn.Linear(16 * 5 * 5, 120)
+            #self.pixels_after_conv = (int)((28/_pool_size)*(28/_pool_size)*_out)
+            self.pixels_after_conv = (int)((_input_size[1]/_pool_size)*(_input_size[2]/_pool_size)*_out)
+            self.fc1 = nn.Linear(self.pixels_after_conv, 120)
+    #         self.fc1 = nn.Linear(-1, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 10)
+        def forward(self, x):
+            debug = False  
+            if debug: print('1',x.shape) # [4, 3, 28, 28]) 4->BatchSize
+            x = self.pool(F.relu(self.conv1(x))) 
+            if debug: print('2',x.shape) # [4, 6, 14, 14])
+            #x = x.view(-1,self.pixels_after_conv) 
+            x = x.view(x.shape[0],-1)  # x.shape[0] = 4 (batch size), -1 => WxHxC
+            if debug: print('3',x.shape) # [4, 1176] # reshape
+            x = F.relu(self.fc1(x)) 
+            if debug: print('4',x.shape) # [4, 120]
+            x = F.relu(self.fc2(x)) 
+            if debug: print('5',x.shape) # [4, 84]
+            x = self.fc3(x) 
+            if debug: print('6',x.shape) # [4, 10] 4->BatchSize !!!
+            return x
+
+    # model = _resnet18(pretrained=pretrained, **kwargs)
+    # net = Net(_input_size=iter(trainloader).next()['image'].shape[1:]) #(4,3,28,28)->(3,28,28)
+    net = Net(_input_size=(3,28,28)) #(4,3,28,28)->(3,28,28)
+    model = net
+    return model
